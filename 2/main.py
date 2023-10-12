8025f2e3017a9538ffaaae48250ca5d4ac5e88a9
Koki Kiyota <82688821+kkiyota63@users.noreply.github.com>
2023-10-12 10:57:19 +0900

    update

--- 2/main.py
+++ 2/main.py
@@ -1,9 +1,24 @@
-#turtleをインポートする
 from turtle import *
-# 以下に五角形を書く
-for i in range (5):
-    forward(100)
-    left(72)
-done()
 
+def koch_snowflake_recursion(order, size):
+    #分岐
+    if order == 0:
+        forward(size)
+    else:
+        for angle in [60, -120, 60, 0]:
+            #再起呼び出し
+            koch_snowflake_recursion(order-1, size/3)
+            left(angle)
 
+def draw_koch_snowflake(order=3, size=200):
+
+    # 雪片を3回描画して、完全なKochの雪片を形成する
+    #繰り返し
+    for _ in range(3):
+        koch_snowflake_recursion(order, size)
+        right(120)
+
+    done()
+
+#描画
+draw_koch_snowflake()
