c6ce372f877ea053218c3d793af9f095ae9a3983
Koki Kiyota <82688821+kkiyota63@users.noreply.github.com>
2023-10-12 09:08:09 +0900

    fix

--- 2/main.py
+++ 2/main.py
@@ -1,12 +1,9 @@
 #turtleをインポートする
 from turtle import *
-# 以下に図形を書く
-forward(100)
-left(90)
-forward(100)
-left(90)
-forward(100)
-left(90)
-forward(100)
-#turtleを終了する
+# 以下に五角形を書く
+for i in range (5):
+    forward(100)
+    left(72)
 done()
+
+
